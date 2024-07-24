7b57d8b8443ab37bcbce152dccbac7ad892b8ab2
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-07-24 23:42:20 +0900

    chore: 자잘한 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Lable from './componet/atoms/Label/Lable'
+
 function App() {
-  return <div className="App">안녕</div>
+  return (
+    <div className={'App'}>
+      <Lable variant={'primary'}>{'안녕'}</Lable>
+    </div>
+  )
 }
 
 export default App
