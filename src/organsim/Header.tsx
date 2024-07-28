ee9f7b0a66467b625a0320b1ec83de2337c12ec0
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-07-28 15:35:51 +0900

    feat: Header 컴포넌트 작성

---
+++ src/organsim/Header.tsx
@@ -0,0 +1,14 @@
+import Lable from '../componet/atoms/Label/Lable'
+
+function Header() {
+  return (
+    <header>
+      <Lable variant={'primary'}>{'선물 개봉'}</Lable>
+      <Lable variant={'primary'}>{'기념 사진'}</Lable>
+      <Lable variant={'primary'}>{'안녕'}</Lable>
+      <Lable variant={'primary'}>{'안녕'}</Lable>
+    </header>
+  )
+}
+
+export default Header
