f33b58bb3876dc64319660ec5f01fbd19df2c76c
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-07-28 15:38:26 +0900

    chore : 헤더 내용 수정

--- src/organsim/Header.tsx
+++ src/organsim/Header.tsx
@@ -3,10 +3,10 @@ import Lable from '../componet/atoms/Label/Lable'
 function Header() {
   return (
     <header>
-      <Lable variant={'primary'}>{'선물 개봉'}</Lable>
+      <Lable variant={'primary'}>{'추억의 순간'}</Lable>
       <Lable variant={'primary'}>{'기념 사진'}</Lable>
-      <Lable variant={'primary'}>{'안녕'}</Lable>
-      <Lable variant={'primary'}>{'안녕'}</Lable>
+      <Lable variant={'primary'}>{'편지 쓰기'}</Lable>
+      <Lable variant={'primary'}>{'선물 개봉'}</Lable>
     </header>
   )
 }
