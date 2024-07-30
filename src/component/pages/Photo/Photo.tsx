6d538faf360d32e2b1329dba98ab2aa3e99e7b2c
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-07-30 17:31:54 +0900

    feat : 웹캠 세팅

--- src/component/pages/Photo/Photo.tsx
+++ src/component/pages/Photo/Photo.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Webcam from 'react-webcam'
 import Title from '../../atoms/Title/Title'
 
 function Photo() {
-  return <Title variant={'primary'}>{'사진 촬영!'}</Title>
+  const webcamRef = useRef(null)
+
+  return (
+    <>
+      <Title variant={'primary'}>{'사진 촬영!'}</Title>
+      <Webcam ref={webcamRef} screenshotFormat={'image/jpeg'} width={640} height={480} />
+    </>
+  )
 }
 
 export default Photo
