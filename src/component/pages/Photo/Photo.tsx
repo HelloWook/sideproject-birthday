5b8bf1849e6f288b3783d4dc3e9b5ef457c7da28
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-07-30 17:16:29 +0900

    chore : react-webcam 라이브러리 설치

--- src/component/pages/Photo/Photo.tsx
+++ src/component/pages/Photo/Photo.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import Title from '../../atoms/Title/Title'
-import Tooltip from '../../atoms/Tooltip/Tooltip'
 
 function Photo() {
-  return (
-    <Tooltip message={'ㅎㅇ'}>
-      <Title variant={'primary'}>{'사진 촬영!'}</Title>
-    </Tooltip>
-  )
+  return <Title variant={'primary'}>{'사진 촬영!'}</Title>
 }
 
 export default Photo
