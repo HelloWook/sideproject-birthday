91290cbce3891ce3e739032713b0250bb2a9d8de
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-07-31 23:38:47 +0900

    refacotr : 모달 훅 리펙토링

--- src/component/pages/Letter/Letter.tsx
+++ src/component/pages/Letter/Letter.tsx
@@ -1,22 +1,17 @@
 import Title from '../../atoms/Title/Title'
 import Image from '../../atoms/Image/image'
 import useModal from '../../../hooks/useModal'
-import Modal from '../../molecule/Modal'
 import Tooltip from '../../atoms/Tooltip/Tooltip'
 
 function Letter() {
-  const { isOpen, openModal, closeModal } = useModal()
+  const { openModal, ModalComponent } = useModal()
 
   return (
     <div>
       <Tooltip message={'편지를 클릭해주세요'}>
         <Title variant={'primary'}>{'감사한 마음을 전해보아요'}</Title>
         <Image url={'letter.png'} alt={'편지'} onClick={openModal} />
-        {isOpen && (
-          <Modal title={'편지 작성'} clickEvent={closeModal}>
-            {'모달 내용'}
-          </Modal>
-        )}
+        <ModalComponent title={'편지 작성'}>{'모달 내용'}</ModalComponent>
       </Tooltip>
     </div>
   )
--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,15 +1,23 @@
 import { useState } from 'react'
+import Modal from '../component/molecule/Modal'
 
 function useModal() {
   const [isOpen, setIsOpen] = useState<boolean>(false)
 
   const openModal = () => setIsOpen(true)
   const closeModal = () => setIsOpen(false)
+  const ModalComponent = ({ title, children }: { title: string; children: React.ReactNode }) =>
+    isOpen ? (
+      <Modal title={title} clickEvent={closeModal}>
+        {children}
+      </Modal>
+    ) : null
 
   return {
     isOpen,
     openModal,
     closeModal,
+    ModalComponent,
   }
 }
 
