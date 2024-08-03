2fd6f1899a48092acdfd097ba142589ebf3fb87e
남정욱 <113816822+HelloWook@users.noreply.github.com>
2024-08-03 13:20:57 +0900

    feat : 캡쳐시 폭죽 나오기

--- src/component/molecule/Capture/Caputure.tsx
+++ src/component/molecule/Capture/Caputure.tsx
@@ -1,23 +1,34 @@
 import React, { useRef, useState } from 'react'
 import Webcam from 'react-webcam'
 import styled from '@emotion/styled'
+import confetti from 'canvas-confetti'
 import Camera from '../../atoms/WebCam/Camera'
 import Button from '../../atoms/Button/Button'
 
+const CaptuerStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
 function Caputure() {
   const [image, setImage] = useState<string | null>(null)
-
-  const CaptuerStyled = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  `
 
   const capture = () => {
     if (webcamRef.current) {
       const imageSrc = webcamRef.current.getScreenshot()
       setImage(imageSrc)
       console.log(image)
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { x: 0.4, y: 0.6 },
+      })
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { x: 0.6, y: 0.6 },
+      })
     }
   }
 
